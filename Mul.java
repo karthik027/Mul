cf7b075de0d2f6d44c5d047e95eebdad7612830b
karthik027 <senthurmurugan3234@gmail.com>
2017-07-10 12:02:19 +0530

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class Mul
+{
+  public static void main(String args[])
+  {
+    int z=1;
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int mul=a;
+    String b=String.valueOf(a);
+    int l=b.length();
+    char[] c=b.toCharArray();
+    for(int i=0;i<l;i++)
+    {
+      int k=c[i]-48;
+      //System.out.println(k);
+      z=z*k;
+    }
+    mul=mul*z;
+    System.out.println(mul);
+  }
+}
